f481e11035147bb7fa6408a8134d7bd268f23599
Fabio Norbutat <fabio.norbutat@brandung.de>
2023-07-07 09:36:45 +0200

    [TASK] init git

---
+++ Init.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.8
+# -*- coding: utf-8 -*-
+
+import os, logging, sys
+from dotenv import load_dotenv
+from Crawler import Crawler
+from ChatGPT import ChatGPT
+
+import time
+
+load_dotenv()
+
+start_time = time.time()
+print('Start Crawler')
+crawler = Crawler("https://docs.typo3.org/m/typo3/reference-coreapi/11.5/en-us/ApiOverview/DependencyInjection/Index.html", "DependencyInjection")
+
+content = crawler.get_content_from_api()
+print('Save Content in file')
+crawler.save_content(content)
+
+print('Get Content from file')
+content = crawler.get_content()
+
+print('Start ChatGPT')
+chatgpt = ChatGPT(content)
+ChatGPT.create_questions(chatgpt,'DependencyInjection')
+
+end_time = time.time()
+print("END | Time elapsed: " + str(end_time - start_time) + " seconds")
+
+chatGPT chatgpt docs multiple choice questions generator
